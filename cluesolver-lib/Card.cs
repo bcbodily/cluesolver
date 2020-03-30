0458dc0eb3e1a696ed4e9d1b19681c7291a90e75
Brad Bodily <bradbodily@hotmail.com>
2020-03-30 16:48:15 -0600

    Added IComparable<Card> to Card

--- cluesolver-lib/Card.cs
+++ cluesolver-lib/Card.cs
@@ -5,7 +5,7 @@ namespace cluesolver
     /// <summary>
     /// Represents a single card, with a category and title
     /// </summary>
-    public struct Card : IEquatable<Card>
+    public struct Card : IEquatable<Card>, IComparable<Card>
     {
         /// <summary>
         /// The category of the card
@@ -30,17 +30,49 @@ namespace cluesolver
             Title = title;
         }
 
+        /// <summary>
+        /// Returns whether another <see cref="Card"/> is equal to this value
+        /// </summary>
+        /// <param name="other">another <see cref="Card"/> to compare to this value</param>
+        /// <returns>true if the other value is equal to this value; otherwise, false</returns>
         public bool Equals(Card other) =>
             this == other;
 
+        /// <summary>
+        /// Returns a hash code for this value
+        /// </summary>
+        /// <returns>a hash code value consistent for this value</returns>
         public override int GetHashCode() =>
             HashCode.Combine(Category, Title);
 
+        /// <summary>
+        /// Returns whether another <see cref="object"/> is a <see cref="Card"/> equal to this value
+        /// </summary>
+        /// <param name="obj">an object to compare to this value</param>
+        /// <returns>true if the object is a <see cref="Card"/> equal to this value; otherwise, false</returns>
         public override bool Equals(object obj) =>
             obj is Card other &&
             this == other;
 
-        public override string ToString() => Title;
+        /// <summary>
+        /// Returns a string representation of this value
+        /// </summary>
+        /// <returns>a string in the following format: <code>Category:Title</code></returns>
+        public override string ToString() => $"{Category}:{Title}";
+
+        /// <summary>
+        /// Compares this value with a specified <see cref="Card"/> and indicates whether this value preceds, follows, or appears in the same position in the sort order as ths specified <see cref="Card"/>
+        /// </summary>
+        /// <param name="other">the <see cref="Card"/> to compare with this value</param>
+        /// <returns>A value less than zero if this value precedes the specified value, or a value greater than zero if this value follows the specified value; otherwise, zero</returns>
+        public int CompareTo(Card other)
+        {
+            var catCompare = Category.CompareTo(other.Category);
+            if (catCompare != 0) return catCompare;
+
+            // else
+            return Title.CompareTo(other.Title);
+        }
 
         public static bool operator ==(Card lhs, Card rhs) =>
             lhs.Category.Equals(rhs.Category) &&
--- cluesolver-test/CardTest.cs
+++ cluesolver-test/CardTest.cs
@@ -6,6 +6,122 @@ namespace cluesolver
     public class CardTest
     {
         /// <summary>
+        /// Verifies <see cref="Card.CompareTo(Card)"/>, when <see cref="Card.Category"/> for the comparing value follows <see cref="Card.Category"/> for the value being compared to, properly returns a value greater than 0
+        /// </summary>
+        /// <param name="category1">the category to assign to the first value</param>
+        /// <param name="category2">the category to assign to the second value</param>
+        [Theory]
+        [InlineData("b", "a")]
+        [InlineData("c", "b")]
+        public void CompareTo_category_follows_returns_greater_than_zero(string category1, string category2)
+        {
+            var card1 = new Card(category: category1, title: "title");
+            var card2 = new Card(category: category2, title: "title");
+
+            Assert.True(category1.CompareTo(category2) > 0);
+
+            var actual = card1.CompareTo(card2) > 0;
+            var expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.CompareTo(Card)"/>, when <see cref="Card.Category"/> for the comparing value appears in the same position as the value being compared to, and <see cref="Card.Title"/> for the comparing value appears in the same position as the value being compared to, properly returns 0
+        /// </summary>
+        /// <param name="title">the title to assign to both values</param>
+        [Theory]
+        [InlineData("a")]
+        [InlineData("b")]
+
+        public void CompareTo_Category_is_same_Title_is_same_returns_zero(string title) {
+            var category = "category";
+            
+            var card1 = new Card(category: category, title: title);
+            var card2 = new Card(category: category, title: title);
+
+            Assert.True(card1.Category.CompareTo(card2.Category) == 0);
+
+            Assert.True(card1.Title.CompareTo(card2.Title) == 0);
+
+            var actual = card1.CompareTo(card2) == 0;
+            var expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.CompareTo(Card)"/>, when <see cref="Card.Category"/> for the comparing value appears in the same position as the value being compared to, and <see cref="Card.Title"/> for the comparing value follows the value being compared to, properly returns a value greater than 0
+        /// </summary>
+        /// <param name="title1">the title to assign to the first value</param>
+        /// <param name="title2">the title to assign to the second value</param>
+        [Theory]
+        [InlineData("b", "a")]
+        [InlineData("c", "b")]
+
+        public void CompareTo_Category_is_same_Title_follows_returns_greater_than_zero(string title1, string title2) {
+            var category = "category";
+            
+            var card1 = new Card(category: category, title: title1);
+            var card2 = new Card(category: category, title: title2);
+
+            Assert.True(card1.Category.CompareTo(card2.Category) == 0);
+
+            Assert.True(card1.Title.CompareTo(card2.Title) > 0);
+
+            var actual = card1.CompareTo(card2) > 0;
+            var expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.CompareTo(Card)"/>, when <see cref="Card.Category"/> for the comparing value appears in the same position as the value being compared to, and <see cref="Card.Title"/> for the comparing value precedes the value being compared to, properly returns a value less than 0
+        /// </summary>
+        /// <param name="title1">the title to assign to the first value</param>
+        /// <param name="title2">the title to assign to the second value</param>
+        [Theory]
+        [InlineData("a", "b")]
+        [InlineData("b", "c")]
+
+        public void CompareTo_Category_is_same_Title_precedes_returns_less_than_zero(string title1, string title2) {
+            var category = "category";
+            
+            var card1 = new Card(category: category, title: title1);
+            var card2 = new Card(category: category, title: title2);
+
+            Assert.True(card1.Category.CompareTo(card2.Category) == 0);
+
+            Assert.True(card1.Title.CompareTo(card2.Title) < 0);
+
+            var actual = card1.CompareTo(card2) < 0;
+            var expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.CompareTo(Card)"/>, when <see cref="Card.Category"/> for the comparing value precedes <see cref="Card.Category"/> for the value being compared to, properly returns a value less than 0
+        /// </summary>
+        /// <param name="category1">the category to assign to the first value</param>
+        /// <param name="category2">the category to assign to the second value</param>
+        [Theory]
+        [InlineData("a", "b")]
+        [InlineData("b", "c")]
+        public void CompareTo_category_precedes_returns_less_than_zero(string category1, string category2)
+        {
+            var card1 = new Card(category: category1, title: "title");
+            var card2 = new Card(category: category2, title: "title");
+
+            Assert.True(category1.CompareTo(category2) < 0);
+
+            var actual = card1.CompareTo(card2) < 0;
+            var expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
         /// Verifies <see cref="Card.Card(string, string)"/> properly sets <see cref="Card.Category"/> to the category value specified
         /// </summary>
         [Fact]
@@ -36,10 +152,74 @@ namespace cluesolver
         }
 
         /// <summary>
-        /// Verifies <see cref="Card.Equals(object)"/>, when the other value is an <see cref="Object"/> that is not a <see cref="Card"/>, properly returns false
+        /// Verifies <see cref="Card.Equals(Card)"/>, when <see cref="Card.Category"/> and <see cref="Card.Title"/> for the other value are equal, properly returns true
         /// </summary>
         [Fact]
-        public void Equals_other_is_object_and_not_Card_returns_false() {
+        public void Equals_other_is_card_and_equal_returns_true()
+        {
+            var title = "title";
+            var category = "category";
+
+            var instance = new Card(category: category, title: title);
+            var other = new Card(category: category, title: title);
+
+            Assert.Equal(other.Category, instance.Category);
+            Assert.Equal(other.Title, instance.Title);
+
+            var actual = instance.Equals(other);
+            var expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.Equals(Card)"/>, when <see cref="Card.Category"/> for the other value is not equal, properly returns false
+        /// </summary>
+        [Fact]
+        public void Equals_other_is_card_Category_isNot_equal_returns_false()
+        {
+            var title = "title";
+            var category1 = "category1";
+            var category2 = "category2";
+
+            var instance = new Card(category: category1, title: title);
+            var other = new Card(category: category2, title: title);
+
+            Assert.NotEqual(other.Category, instance.Category);
+
+            var actual = instance.Equals(other);
+            var expected = false;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.Equals(Card)"/>, when <see cref="Card.Title"/> for the other value is not equal, properly returns false
+        /// </summary>
+        [Fact]
+        public void Equals_other_is_card_Title_isNot_equal_returns_false()
+        {
+            var title1 = "title1";
+            var title2 = "title2";
+            var category = "category";
+
+            var instance = new Card(category: category, title: title1);
+            var other = new Card(category: category, title: title2);
+
+            Assert.NotEqual(other.Title, instance.Title);
+
+            var actual = instance.Equals(other);
+            var expected = false;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.Equals(object)"/>, when the other value is an <see cref="object"/> that is not a <see cref="Card"/>, properly returns false
+        /// </summary>
+        [Fact]
+        public void Equals_other_is_object_and_not_Card_returns_false()
+        {
             Object other = new Object();
             var instance = new Card();
 
@@ -51,8 +231,35 @@ namespace cluesolver
             Assert.Equal(expected, actual);
         }
 
+        /// <summary>
+        /// Verifies <see cref="Card.Equals(object)"/> when the other value is an <see cref="object"/> that is a <see cref="Card"/> and is equal, properly returns true
+        /// </summary>
         [Fact]
-        public void Equals_other_is_object_and_Card_and_not_equal_returns_false() {
+        public void Equals_other_is_object_and_Card_and_is_equal_returns_true()
+        {
+            var title = "title1";
+            var category = "category";
+
+            var instance = new Card(category: category, title: title);
+            var card2 = new Card(category: category, title: title);
+
+            Object other = card2;
+
+            Assert.True(other is Card);
+            Assert.Equal(instance, card2);
+
+            var actual = instance.Equals(other);
+            var expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.Equals(object)"/> when the other value is an <see cref="object"/> that is a <see cref="Card"/> and is not equal, properly returns false
+        /// </summary>
+        [Fact]
+        public void Equals_other_is_object_and_Card_and_not_equal_returns_false()
+        {
             var title1 = "title1";
             var title2 = "title2";
             var category = "category";
@@ -67,6 +274,29 @@ namespace cluesolver
 
             var actual = instance.Equals(other);
             var expected = false;
+
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Card.GetHashCode"/> properly returns a consistent hash code for equal <see cref="Card"/> values
+        /// </summary>
+        /// <param name="category">the value to assign to <see cref="Card.Category"/> for each <see cref="Card"/> value</param>
+        /// <param name="title">the value to assign to <see cref="Card.Title"/> for each <see cref="Card"/> value</param>
+        [Theory]
+        [InlineData("cat1", "title1")]
+        [InlineData("cat2", "title1")]
+        [InlineData("cat1", "title2")]
+        [InlineData("cat2", "title2")]
+        public void GetHashCode_returns_same_code_for_same_values(string category, string title)
+        {
+            var card1 = new Card(category: category, title: title);
+            var card2 = new Card(category: category, title: title);
+
+            Assert.Equal(card2, card1);
+
+            var actual = card2.GetHashCode();
+            var expected = card1.GetHashCode();
 
             Assert.Equal(expected, actual);
         }
@@ -203,5 +433,19 @@ namespace cluesolver
             Assert.Equal(expected, actual);
         }
 
+        /// <summary>
+        /// Verifies <see cref="Card.ToString"/> properly returns a string in the following format: <code>Category:Title</code>
+        /// </summary>
+        [Fact]
+        public void ToString_returns_Title()
+        {
+            var category = "category";
+            var title = "title";
+
+            var actual = new Card(category: category, title: title).ToString();
+            var expected = $"{category}:{title}";
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
